ff50ef566a8505db69949f09758ac6d6c33e96bc
alsaeedd <saeedalsaeedbusiness@gmail.com>
2025-02-13 10:59:01 +0300

    Spring setup complete

---
+++ src/test/java/com/sm/conference_management/conference_management/ConferenceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.conference_management.conference_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
